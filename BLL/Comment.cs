d6f2182f8b6f48c3c8b50530c0b8921ddf17671f
emnigma <nigmatulin.maxim.v@yandex.ru>
2019-12-17 02:40:08 +0300

    second (no links)

---
+++ BLL/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Comment
+    {
+        public int Id { get; set; }
+        public string text { get; set; }
+        public DateTime Created { get; set; }
+
+    }
+}
---
+++ BLL/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+        public bool IsCommentsClosed { get; set; }
+
+    }
+}
